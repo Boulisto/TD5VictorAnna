d5878843247a544b9f1de4e19cc63c3369f8d71c
Victor Dufour <victorduf@outlook.fr>
2021-03-29 23:12:12 +0200

    question 5

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from book import Book
+from pandas import pd
 def main():
 	book = Book("TEST")
 	book.insert_buy(10, 10.0)
@@ -8,6 +9,36 @@ def main():
 	book.insert_buy(2, 11.0)
 	book.insert_sell(1, 10.0)
 	book.insert_sell(10, 10.0)
+	return book
 if __name__ == "__main__":
-	main()
+	book=main()
 
+
+
+
+'''def main():
+      book = Book("TEST")
+      boo=[]
+       
+      book.insert_buy(10, 10.0)
+      book.insert_sell(120, 12.0)
+      book.insert_buy(5, 10.0)
+      book.insert_buy(2, 11.0)
+      book.insert_sell(1, 10.0)
+      book.insert_sell(10, 10.0)
+      for i in book.orders:
+           
+           boo.append(i)
+      return boo
+if __name__ == "__main__":
+        book=main()
+        len(book)
+        """dic={'buy':[],'sell':[]}
+        for i in book:
+            if i[0]=='B':
+                dic['buy'].append(book[i])
+            else:
+                
+             dic['sell'].append(book[i])
+        data=pd.DataFrame[dic]
+        data'''
